02efbbcdad3588843e33539952bcd0e469828833
George Kephart <Gkephart@cnm.edu>
2018-11-21 07:51:18 -0700

    added post.php

--- php/lib/post.php
+++ php/lib/post.php
@@ -1,1 +1,6 @@
+<?php 
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
+//absolute path to YOUR ini file on the server 
+$secrets =  new Secrets("/home/gkephart/gkephart.ini");
+$pdo = $secrets->getPdoObject();
